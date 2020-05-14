0b21fe33106e446bc10349be8e64550cd68f225f
orlanildo <orlanildoaldo@hotmail.com>
2020-05-14 12:24:43 -0300

    start controllers app, login, register, start form register

--- frontEnd/crontrollers/loginController.js
+++ frontEnd/crontrollers/loginController.js
@@ -3,50 +3,58 @@ app.controller('loginController', function ($scope, $http) {
     $scope.client = {}
 
 
-    $scope.listClients = function () {
-        // $http.get('/client').then(function(res){
-        //     console.log(res.data)
-        //     console.log(res.status)
-        // }, function(res){
-        //     console.log(res.data)
-        //     console.log(res.status)
-        // })
-        console.log("listClient")
+    //mandar o email e a senha via post no body para a api
 
-    }
 
-    $scope.createClient = function () {
-        // $http.post('/client').then(function(res){
-        //     console.log(res.data)
-        //     console.log(res.status)
-        // }, function(res){
-        //     console.log(res.data)
-        //     console.log(res.status)
-        // })
-        console.log("createClient")
-    }
-
-    $scope.updateClient = function () {
-        // $http.post('/client/id').then(function(res){
-        //     console.log(res.data)
-        //     console.log(res.status)
-        // }, function(res){
-        //     console.log(res.data)
-        //     console.log(res.status)
-        // })
-        console.log("updateClient")
-    }
-
-    $scope.deleteClient = function () {
-        // $http.post('/client/id').then(function(res){
-        //     console.log(res.data)
-        //     console.log(res.status)
-        // }, function(res){
-        //     console.log(res.data)
-        //     console.log(res.status)
-        // })
-        console.log("deleteClient")
+    $scope.goToRegister = function() {
+        // Redirecionar para a tela de cadastro
+        console.log('goToRegister')
     }
 
 })
 
+
+// $scope.listClients = function () {
+//     // $http.get('/client').then(function(res){
+//     //     console.log(res.data)
+//     //     console.log(res.status)
+//     // }, function(res){
+//     //     console.log(res.data)
+//     //     console.log(res.status)
+//     // })
+//     console.log("listClient")
+
+// }
+
+// $scope.createClient = function () {
+//     // $http.post('/client').then(function(res){
+//     //     console.log(res.data)
+//     //     console.log(res.status)
+//     // }, function(res){
+//     //     console.log(res.data)
+//     //     console.log(res.status)
+//     // })
+//     console.log("createClient")
+// }
+
+// $scope.updateClient = function () {
+//     // $http.post('/client/id').then(function(res){
+//     //     console.log(res.data)
+//     //     console.log(res.status)
+//     // }, function(res){
+//     //     console.log(res.data)
+//     //     console.log(res.status)
+//     // })
+//     console.log("updateClient")
+// }
+
+// $scope.deleteClient = function () {
+//     // $http.post('/client/id').then(function(res){
+//     //     console.log(res.data)
+//     //     console.log(res.status)
+//     // }, function(res){
+//     //     console.log(res.data)
+//     //     console.log(res.status)
+//     // })
+//     console.log("deleteClient")
+// }
--- frontEnd/crontrollers/registerController.js
+++ frontEnd/crontrollers/registerController.js
@@ -1,3 +1,19 @@
 app.controller('registerController', function ($scope, $http) {
 
+
+    $scope.createClient = function () {
+        // $http.post('/client').then(function(res){
+        //     console.log(res.data)
+        //     console.log(res.status)
+        // }, function(res){
+        //     console.log(res.data)
+        //     console.log(res.status)
+        // })
+        console.log("createClient")
+    }
+
+    $scope.cancel = function() {
+        //Redirecionar para o login
+        console.log('cancel')
+    }
 })
